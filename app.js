88936d253096047852bcc2eaaffd2fd6a87af14a
Johan Schuijt <johan@publicpixels.nl>
2019-04-17 14:16:57 +0200

    move ain2 config to module config

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 
 var pm2       = require('pm2');
 var SysLogger = require('ain2');
-var logger    = new SysLogger({tag: 'pm2',  facility: 'syslog'});
 
-pm2.launchBus(function(err, bus) {
-  bus.on('*', function(event, data){
-    if (event == 'process:event') {
-      logger.warn('app=pm2 target_app=%s target_id=%s restart_count=%s status=%s',
-                  data.process.name,
-                  data.process.pm_id,
-                  data.process.restart_time,
-                  data.event);
-    }
-  });
+pmx.initModule({}, function(err, config) {
+	var logger = new SysLogger(config);
 
-  bus.on('log:err', function(data) {
-    logger.error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
-  });
+	pm2.launchBus(function(err, bus) {
+	  bus.on('*', function(event, data){
+		if (event == 'process:event') {
+		  logger.warn('app=pm2 target_app=%s target_id=%s restart_count=%s status=%s',
+					  data.process.name,
+					  data.process.pm_id,
+					  data.process.restart_time,
+					  data.event);
+		}
+	  });
 
-  bus.on('log:out', function(data) {
-    logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
-  });
+	  bus.on('log:err', function(data) {
+		logger.error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+	  });
+
+	  bus.on('log:out', function(data) {
+		logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+	  });
+	});
 });
+
+
